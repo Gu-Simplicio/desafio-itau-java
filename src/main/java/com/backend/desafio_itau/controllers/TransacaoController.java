1d981e2a4a0aa3e6e0889caae115b67065779af8
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-05-01 15:46:13 -0300

    feat(estrutura-base): estrutura base de pastas e arquivos que envolvem o objeto Transacao criada

--- src/main/java/com/backend/desafio_itau/controllers/TransacaoController.java
+++ src/main/java/com/backend/desafio_itau/controllers/TransacaoController.java
@@ -1,5 +1,10 @@
 package com.backend.desafio_itau.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/unibanco")
 public class TransacaoController {
     
 }
---
+++ src/main/java/com/backend/desafio_itau/services/TransacaoService.java
@@ -0,0 +1,8 @@
+package com.backend.desafio_itau.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransacaoService {
+    
+}
