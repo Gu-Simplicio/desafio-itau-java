f4392ad95fcb6ca1083992724a27695eefbc9da3
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-05-02 00:00:29 -0300

    feat(/transacao): Controller do endpoint POST /transacao criado e ligado à TransacaoService

--- src/main/java/com/backend/desafio_itau/controllers/TransacaoController.java
+++ src/main/java/com/backend/desafio_itau/controllers/TransacaoController.java
@@ -1,10 +1,38 @@
 package com.backend.desafio_itau.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.desafio_itau.model.Transacao;
+import com.backend.desafio_itau.services.TransacaoService;
 
 @RestController
 @RequestMapping("/unibanco")
 public class TransacaoController {
-    
+    @Autowired
+    TransacaoService transacaoService;
+
+    // cria uma nova transação
+    @PostMapping("/transacao") 
+    public ResponseEntity<Void> criaTransacao(@RequestBody Transacao transacao){ 
+        // caso a transação enviada seja inválida
+        if(transacao.getValor() < 0 || transacao.getDataHora() == null){
+            System.err.println("Valor inválido enviado na requisição");
+            return ResponseEntity.unprocessableContent().build();
+        }
+        // recebe um valor caso a transação seja bem sucedida
+        boolean transacaoCriada = transacaoService.criaTransacao(transacao);
+
+        if(!transacaoCriada) {
+            System.err.println("Erro ao salvar transação");
+            return ResponseEntity.badRequest().build();
+        }
+        System.err.println("Transação salva com sucesso!");
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
