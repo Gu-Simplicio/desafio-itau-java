19706a4e2bff4c0f648475acc6ab0ea658ca9aaf
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-05-02 02:47:58 -0300

    test(service /transacao): testes unitários do Service do endpoint Post /transacao criados e funcionais

---
+++ src/test/java/com/backend/desafio_itau/service/TransacaoServiceTest.java
@@ -0,0 +1,41 @@
+package com.backend.desafio_itau.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.jupiter.api.Test;
+
+import com.backend.desafio_itau.model.Transacao;
+
+public class TransacaoServiceTest {
+    private TransacaoService transacaoService = new TransacaoService();
+
+    @Test // CASO PERFEITO
+    public void criaTransacao_RetornaTrue(){
+        // cria uma transação para testar a criação
+        Transacao transacao = new Transacao();
+        transacao.setValor(0.15);
+        transacao.setDataHora(OffsetDateTime.now());
+
+        // executa e testa
+        boolean resultado = transacaoService.criaTransacao(transacao);
+        assertEquals(true, resultado);
+    }
+
+    @Test // DATAHORA DO FUTURO
+    public void criaTransacao_DataHoraRetornaFalse() {
+        // CHECAGEM DE dataHora FUTURA
+        OffsetDateTime dataFutura = OffsetDateTime.now().plus(1, ChronoUnit.DAYS); // cria data 1 dia no futuro
+
+        // cria a transação no futuro
+        Transacao transacao = new Transacao();
+        transacao.setValor(0);
+        transacao.setDataHora(dataFutura);
+
+        // executa e testa
+        boolean resultado = transacaoService.criaTransacao(transacao);
+        assertEquals(false, resultado);
+    }
+}
