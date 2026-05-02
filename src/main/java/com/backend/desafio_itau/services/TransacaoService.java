2c7bceee265cfb347de0f40d4f95cdca980f263b
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-05-01 23:59:50 -0300

    feat(POST): Service do endpoint POST /transacao criado, funcional e com os erros sendo checados

--- src/main/java/com/backend/desafio_itau/services/TransacaoService.java
+++ src/main/java/com/backend/desafio_itau/services/TransacaoService.java
@@ -1,8 +1,30 @@
 package com.backend.desafio_itau.services;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.backend.desafio_itau.model.Transacao;
 
 @Service
 public class TransacaoService {
-    
+    // Lista de todas as transações já salvas
+    private List<Transacao> transacoes = new ArrayList<Transacao>();
+
+    public boolean criaTransacao(Transacao transacao){
+        // checa se dataHora é válido   
+        OffsetDateTime now = OffsetDateTime.now();
+        if(transacao.getDataHora().isAfter(now)){
+            System.err.println("dataHora do futuro detectada");
+            return false;
+        }
+
+        // CASO ESTEJA TUDO CERTO!
+        transacoes.add(transacao);
+        System.out.println("Transação salva!");
+        System.out.println("Tamanho atual da lista: " + transacoes.size());
+        return true;
+    }
 }
