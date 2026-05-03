a6d5f64d9b3673a94d9685400debb38a0013bb3d
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-05-03 16:07:30 -0300

    feat(Estatisticas): classe Estatisticas criada, junto ao construtor, getters e setters

---
+++ src/main/java/com/backend/desafio_itau/model/Estatisticas.java
@@ -0,0 +1,59 @@
+package com.backend.desafio_itau.model;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+public class Estatisticas {
+    Double count;
+    Double sum;
+    Double avg;
+    Double max;
+    Double min;
+
+    public Estatisticas(List<Double> valoresTransicionados){
+        // cria um obj de estatísticas e insere os valores (já separados pelo tempo)
+        DoubleSummaryStatistics estatisticasValores = new DoubleSummaryStatistics();
+        for(Double valor : valoresTransicionados) estatisticasValores.accept(valor);
+
+        // salva todas as estatísticas
+        this.setCount(Double.valueOf(estatisticasValores.getCount()));
+        this.setSum(Double.valueOf(estatisticasValores.getSum()));
+        this.setAvg(Double.valueOf(estatisticasValores.getAverage()));
+        this.setMax(Double.valueOf(estatisticasValores.getMax()));
+        this.setMin(Double.valueOf(estatisticasValores.getMin()));
+    }
+
+    // GETTERS
+    public Double getCount(){
+        return this.count;
+    }
+    public Double getSum(){
+        return this.sum;
+    }
+    public Double getAvg(){
+        return this.avg;
+    }
+    public Double getMax(){
+        return this.max;
+    }
+    public Double getMin(){
+        return this.min;
+    }
+
+    // SETTERS
+    public void setCount(Double count) {
+        this.count = count;
+    }
+    public void setSum(Double sum) {
+        this.sum = sum;
+    }
+    public void setAvg(Double avg) {
+        this.avg = avg;
+    }
+    public void setMax(Double max) {
+        this.max = max;
+    }
+    public void setMin(Double min) {
+        this.min = min;
+    }
+}
