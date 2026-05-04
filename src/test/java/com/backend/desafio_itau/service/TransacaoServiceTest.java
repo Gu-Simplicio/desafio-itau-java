828699f3b7f79f7c26bc90a25c5da1b628584230
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-05-04 01:59:18 -0300

    testRefactor: testes unitários de TransacaoService atualizados

--- src/test/java/com/backend/desafio_itau/service/TransacaoServiceTest.java
+++ src/test/java/com/backend/desafio_itau/service/TransacaoServiceTest.java
@@ -1,6 +1,7 @@
 package com.backend.desafio_itau.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
@@ -14,31 +15,31 @@ public class TransacaoServiceTest {
     private TransacaoService transacaoService = new TransacaoService();
 
     // TESTES POST
-    @Test // caso perfeito
-    public void criaTransacao_RetornaTrue(){
+    
+    @Test // valor negativo
+    public void criaTransacao_ValorNegativo(){
+        Transacao transacao = new Transacao();
+        transacao.setValor(-5.10);
+        transacao.setDataHora(OffsetDateTime.now());
+
+        // espera um erro de argumento
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> transacaoService.criaTransacao(transacao));
+    }
+
+    @Test // caso dataHora no futuro
+    public void criaTransacao_DataHoraFutura(){
+        OffsetDateTime dataFutura = OffsetDateTime.now().plusDays(5);
         // cria uma transação para testar a criação
         Transacao transacao = new Transacao();
         transacao.setValor(0.15);
-        transacao.setDataHora(OffsetDateTime.now());
-
-        // executa e testa
-        boolean resultado = transacaoService.criaTransacao(transacao);
-        assertEquals(true, resultado);
-    }
-
-    @Test // dataHora no futuro
-    public void criaTransacao_DataHoraRetornaFalse() {
-        // CHECAGEM DE dataHora FUTURA
-        OffsetDateTime dataFutura = OffsetDateTime.now().plus(1, ChronoUnit.DAYS); // cria data 1 dia no futuro
-
-        // cria a transação no futuro
-        Transacao transacao = new Transacao();
-        transacao.setValor(0);
         transacao.setDataHora(dataFutura);
 
-        // executa e testa
-        boolean resultado = transacaoService.criaTransacao(transacao);
-        assertEquals(false, resultado);
+        // expera receber um erro do tipo IllegalArgumentException
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> transacaoService.criaTransacao(transacao));
     }
 
     // TESTES GET
