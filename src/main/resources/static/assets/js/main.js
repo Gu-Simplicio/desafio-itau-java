5145cb7977a4d4ff238a95b667b01fe8572d60a9
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-09-03 03:16:20 -0300

    feat(frontend): agora a mudança de período faz a tabela ser atualizada

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -7,12 +7,21 @@ const form = document.querySelector("form");
 const btnDeletar = document.querySelector("#btnDeletar");
 const radTodas = document.querySelector("#radTodas");
 const radPeriodo = document.querySelector("#radPeriodo");
+const btnPeriodo = document.querySelector("#btnPeriodo");
 
 form.addEventListener("submit", (event) => criaTransacao(event));
 btnDeletar.addEventListener("click", () => deletaTransacoes());
 
 radTodas.addEventListener("change", () => exibeCampoPeriodo(true))
 radPeriodo.addEventListener("change", () => exibeCampoPeriodo(false));
+
+btnPeriodo.addEventListener("click", () => {
+    const inPeriodo = document.querySelector("#inPeriodo");
+    const periodo = inPeriodo.value || 60;
+
+    recebeTransacoes(`estatistica?periodo=${periodo}`);
+
+})
 
 
 recebeTransacoes("transacao");
