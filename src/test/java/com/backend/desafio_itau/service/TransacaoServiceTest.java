844ee0005ef9f1b58b83d87a5988c031f3641c53
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-09-12 18:16:47 -0300

    testes(transacaoService): testes do service atualizados, novo teste de happy path e nova forma de criação de transacoesService

--- src/test/java/com/backend/desafio_itau/service/TransacaoServiceTest.java
+++ src/test/java/com/backend/desafio_itau/service/TransacaoServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.OffsetDateTime;
 
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.backend.desafio_itau.model.Estatisticas;
@@ -12,6 +14,11 @@ import com.backend.desafio_itau.model.Transacao;
 
 public class TransacaoServiceTest {
     private TransacaoService transacaoService = new TransacaoService();
+
+    @BeforeEach
+    void setUpTransacao() {
+        transacaoService = new TransacaoService();
+    }
 
     // TESTES POST
     
@@ -41,6 +48,35 @@ public class TransacaoServiceTest {
             () -> transacaoService.criaTransacao(transacao));
     }
 
+    @Test // caso dataHora seja nulo
+    public void criaTransacao_DataHoraNula() {
+        OffsetDateTime dataFutura = null;
+        // cria uma transação para testar a criação
+        Transacao transacao = new Transacao();
+        transacao.setValor(0.15);
+        transacao.setDataHora(dataFutura);
+
+        // expera receber um erro do tipo IllegalArgumentException
+        assertThrows(
+            NullPointerException.class, 
+            () -> transacaoService.criaTransacao(transacao));
+    }
+
+    @Test // tudo certo
+    public void criaTransacao_tudoCerto() {
+        Transacao novaTransacao = new Transacao();
+        novaTransacao.setValor(10.50);
+        novaTransacao.setDataHora(OffsetDateTime.now());
+        
+        // recebe o tamanho atual da lista de transações
+        int tamanhoAtual = transacaoService.recebeTransacoes().size();
+        transacaoService.criaTransacao(novaTransacao);
+        // recebe o tamanho após a inserção
+        int tamanhoNovo = transacaoService.recebeTransacoes().size();
+
+        assertEquals(tamanhoAtual, tamanhoNovo - 1);
+    }
+
     // TESTES GET
     @Test
     public void recebeEstatisticas_TresValidas(){
@@ -60,22 +96,22 @@ public class TransacaoServiceTest {
 
         Estatisticas estatisticas = transacaoService.recebeEstatisticas(60); // recebe estatísticas dentro de 60 segs
 
-        assertEquals(3.0, estatisticas.getCount());
-        assertEquals(10.0, estatisticas.getAvg());
-        assertEquals(10.0, estatisticas.getMax());
-        assertEquals(10.0, estatisticas.getMin());
-        assertEquals(30.0, estatisticas.getSum());
+        assertEquals(3.0, estatisticas.getCount(), 0.001);
+        assertEquals(10.0, estatisticas.getAvg(), 0.001);
+        assertEquals(10.0, estatisticas.getMax(), 0.001);
+        assertEquals(10.0, estatisticas.getMin(), 0.001);
+        assertEquals(30.0, estatisticas.getSum(), 0.001);
     }
 
     @Test 
     public void recebeEstatisticas_TudoZero(){ // deve retornar todos os valores como 0.0
         Estatisticas estatisticas = transacaoService.recebeEstatisticas(1);
 
-        assertEquals(0.0, estatisticas.getCount());
-        assertEquals(0.0, estatisticas.getAvg());
-        assertEquals(0.0, estatisticas.getMax());
-        assertEquals(0.0, estatisticas.getMin());
-        assertEquals(0.0, estatisticas.getSum());
+        assertEquals(0.0, estatisticas.getCount(), 0.001);
+        assertEquals(0.0, estatisticas.getAvg(), 0.001);
+        assertEquals(0.0, estatisticas.getMax(), 0.001);
+        assertEquals(0.0, estatisticas.getMin(), 0.001);
+        assertEquals(0.0, estatisticas.getSum(), 0.001);
     }
 
     // TESTES DELETE
