f411c33cf9695c5ac34b4037553775a715404343
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-09-12 21:28:38 -0300

    importação desnecessária retirada de transacaoServiceTest

--- src/test/java/com/backend/desafio_itau/service/TransacaoServiceTest.java
+++ src/test/java/com/backend/desafio_itau/service/TransacaoServiceTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.OffsetDateTime;
 
-import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
